dfc133c1cd994e5bafc260bfd64ec99edfc8f7f5
vamos16 <kartasgoksu16@gmail.com>
2026-04-13 00:16:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
-function showLove(img) {
-  const popup = document.getElementById("popup");
-  const popupImg = document.getElementById("popupImg");
+const text = "Seninle geçen her an hayatımın en güzel kısmı... Seni çok seviyorum Göksu ❤️";
+let i = 0;
 
-  popupImg.src = img.src;
-  popup.style.display = "flex";
-
-  popup.onclick = () => {
-    popup.style.display = "none";
-  };
+function typeWriter() {
+  if (i < text.length) {
+    document.getElementById("typing").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typeWriter, 40);
+  }
 }
+
+function startSite() {
+  document.querySelector(".overlay").style.display = "none";
+  document.querySelector(".container").classList.remove("hidden");
+  document.getElementById("music").play();
+  typeWriter();
+}
+
+function love() {
+  alert("Her şeyden çok seni seviyorum ❤️");
+}
+
+// BURAYI DEĞİŞTİR
+const startDate = new Date("2024-01-01");
+
+function updateTime() {
+  const now = new Date();
+  const diff = now - startDate;
+
+  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
+  const minutes = Math.floor((diff / (1000 * 60)) % 60);
+
+  document.getElementById("time").innerText =
+    days + " gün " + hours + " saat " + minutes + " dakika";
+}
+
+setInterval(updateTime, 1000);
