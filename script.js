ea6ed6a9d0c3b7d5e361b0ff2dff1ff89363e661
vamos16 <kartasgoksu16@gmail.com>
2026-04-13 00:01:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-function degistir() {
-  document.getElementById("text").innerHTML =
-    "Site çalışıyor 🚀";
-}
+const form = document.getElementById("form");
+const formCard = document.getElementById("formCard");
+const success = document.getElementById("success");
+
+form.addEventListener("submit", function () {
+
+  setTimeout(() => {
+    formCard.style.display = "none";
+    success.style.display = "block";
+  }, 800);
+
+});
